c2b2bded944047f8ff9a6c5eaa2fb2d1ea65ed6c
pinkogeneral <48274192+pinkogeneral@users.noreply.github.com>
2023-01-29 19:18:40 +0900

    12~14
    12. 간단한 퍼즐 설정하기
    13. 인터넷으로 플레이하기
    14. 플랫폼 트리거 설정하기

---
+++ myunreal/Source/myunreal/PlatformTrigger.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformTrigger.generated.h"
+
+UCLASS()
+class MYUNREAL_API APlatformTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatformTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* TriggerVolume; //editanywhere 하면 포인터를 편집한다는 것이기 때문에 안됨
+};
