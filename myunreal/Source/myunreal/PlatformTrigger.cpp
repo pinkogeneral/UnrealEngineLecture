75d9ca26058caa3cfc3117d9ac7a5badcb9011ae
pinkogeneral <48274192+pinkogeneral@users.noreply.github.com>
2023-02-09 23:51:37 +0900

    15. C++로 오버랩 이벤트 처리하기

--- myunreal/Source/myunreal/PlatformTrigger.cpp
+++ myunreal/Source/myunreal/PlatformTrigger.cpp
@@ -13,13 +13,19 @@ APlatformTrigger::APlatformTrigger()
 	if (!ensure(TriggerVolume != nullptr)) return; 
 	RootComponent = TriggerVolume; //#include "Components/BoxComponent.h" 
 	//언리얼의 null포인터가 전체 편집기를 다운시킬 수 있다. 
+
+
+	// 이벤트 콜벡을 
+
 }
 
 // Called when the game starts or when spawned
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	// 생성자 -> BeginPlay로 옮김 생성자가 완료되기도 전에 클래스의 멤버 메서드를 생성자에서 함수 포인터로 때려박는 건 순서에 맞지 않는다.
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin); //AddDynamic(UserObject)
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -27,5 +33,15 @@ void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("UnActivated"));
 }
 
--- myunreal/Source/myunreal/PlatformTrigger.h
+++ myunreal/Source/myunreal/PlatformTrigger.h
@@ -26,4 +26,12 @@ public:
 private:
 	UPROPERTY(VisibleAnywhere)
 	class UBoxComponent* TriggerVolume; //editanywhere 하면 포인터를 편집한다는 것이기 때문에 안됨
+	
+										/** 무언가가 구체 컴포넌트에 들어설 때 호출 */
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** 무언가가 구체 컴포넌트를 나설 때 호출 */
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
